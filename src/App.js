ba2d96c9850b0995ba37cfac9915cb27d5827114
Marina8989 <marina.mill209@gmail.com>
2021-07-02 20:28:31 -0700

    react from lorum ipsum generator

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@ import React, { useState } from 'react';
 import data from './data';
 
 function App() {
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let amount = parseInt(count);
+        
+        if(count <= 0) {
+          amount = 1;
+        }
+        if(count > data.length) {
+          amount = data.length;
+        }
+     
+        setText(data.slice(0, amount));
+    }
     return (
         <div>
-            <h2>Form...</h2>
+           <h2>Tired of boring lorem ipsum?</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="amount">Paragraph:</label>
+                <input type="number" name="amount" id="amount" value={count} onChange={(e) => setCount(e.target.value)}/>
+                <button type="submit" className="btn">generate</button>
+            </form>
+             {text.map((item, index)=> {
+                   return (
+                       <p key={index}>{item}</p>
+                   )
+             })}
         </div>
     )
 }
