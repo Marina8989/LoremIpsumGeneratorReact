4121b4b17c05698171e13d217eaed3929a02f3fa
Marina8989 <marina.mill209@gmail.com>
2021-07-02 20:07:06 -0700

    form react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import data from './data';
+
+function App() {
+    return (
+        <div>
+            <h2>Form...</h2>
+        </div>
+    )
+}
+
+export default App
